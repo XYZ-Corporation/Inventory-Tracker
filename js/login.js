f6da85141967a5b0acd88a99bfb136724f0990e4
Ryan Lee <ryanl3708@gmail.com>
2023-03-03 17:15:04 +0000

    login/request form and attempt to sync local storage

--- js/login.js
+++ js/login.js
@@ -4,7 +4,29 @@
 let login = document.getElementById('login-button');
 login.addEventListener('click', loginHandler);
 
+let register = document.getElementById('register-button');
+register.addEventListener('click', registration);
+
+let userInfo = localStorage.getItem('userData');
+let parsedUserInfo = JSON.parse(userInfo);
+
+// used to identify which user is accessing in inventory/request pages.
+let identityNum;
+
+function registration(event) {
+  event.preventDefault();
+  document.location.href = '/signup.html';
+}
 function loginHandler(event) {
   event.preventDefault();
-  document.location.href = '/inventory.html';
+  let idInput = document.getElementById('Username').value;
+  let passwordInput = document.getElementById('Password').value;
+
+  for (let i = 0; i < parsedUserInfo.length; i++){
+    if (parsedUserInfo[i].id === idInput && parsedUserInfo[i].password === passwordInput){
+      identityNum = i;
+      localStorage.setItem('userNum', JSON.stringify(identityNum));
+      document.location.href = '/inventory.html';
+    }
+  }
 }
